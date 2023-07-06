5188993865eefc720096601acc56dd0aa704ec5c
remotestack377 <digamber@positronx.io>
2023-07-06 20:25:29 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'product-detail/:id', component: ProductDetailComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: NoPageFoundComponent },
+];
 
 @NgModule({
+  declarations: [
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ProductsComponent,
+    ProductDetailComponent,
+    NoPageFoundComponent,
+  ],
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {}
